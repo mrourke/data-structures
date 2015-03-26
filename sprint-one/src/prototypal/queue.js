5381534b7004ddda536ef0d1f329a1e32c7b7392
Matthew Rourke <mrourke@gmail.com>
2015-03-26 18:33:54 -0400

    Completed queue prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
+  var object = Object.create(queueMethods);
+  object.first = 0;
+  object.firstEmpty = 0;
+  object.storage = {};
+  return object;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  size: function() {
+    return this.firstEmpty - this.first;
+  },
+
+  enqueue: function(value) {
+    this.storage[this.firstEmpty] = value;
+    this.firstEmpty ++;
+  },
+
+  dequeue: function() {
+    if (this.firstEmpty - this.first) {
+      var temp = this.storage[this.first]
+      delete this.storage[this.first]
+      this.first ++;
+      return temp;
+    }
+  }
+
+
+};
 
 
