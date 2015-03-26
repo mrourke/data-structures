e09403525d34a5a372b56a5371db2d11c79da7c2
Matthew Rourke <mrourke@gmail.com>
2015-03-26 19:19:42 -0400

    Completed pseudoclassical implementation of stacks and queues with terrible git practices

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.first = 0;
+  this.firstEmpty = 0;
 };
 
+Queue.prototype.size = function() {
+  return this.firstEmpty - this.first;
+}
 
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.firstEmpty] = value;
+  this.firstEmpty ++;
+}
+
+Queue.prototype.dequeue = function () {
+  if (this.firstEmpty - this.first) {
+    var temp = this.storage[this.first];
+    delete this.storage[this.first];
+    this.first ++;
+    return temp;
+  }
+}
