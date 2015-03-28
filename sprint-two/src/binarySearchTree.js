e38af698309778d51f4ded66baeaafd229c89534
Matthew Rourke <mrourke@gmail.com>
2015-03-28 18:07:46 -0400

    Completed binary tree functions

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,57 @@
 var BinarySearchTree = function(value){
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  var checkTree = function(value, tree) {
+    if(value < tree.value) {
+      if(tree.left === null) {
+        tree.left = new BinarySearchTree(value);
+      } else {
+        checkTree(value, tree.left)
+      }
+    } else if (value > tree.value) {
+      if(tree.right === null) {
+        tree.right = new BinarySearchTree(value);
+      } else {
+        checkTree(value, tree.right);
+      }
+    }
+  };
+  checkTree(value, this);
+};
 
+BinarySearchTree.prototype.contains = function(target) {
+  var found = false;
+  var recurse = function(target, tree) {
+    if (target === tree.value) {
+      found = true;
+    }
+    if (target < tree.value && tree.left !== null) {
+      recurse(target, tree.left);
+    }
+    if (target > tree.value && tree.right !== null) {
+      recurse(target, tree.right);
+    }
+  };
+  recurse(target, this);
+  return found;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+
+  callback(this.value);
+
+  if (this.left !== null) {
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(callback);
+  }
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
